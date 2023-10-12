f7ed5f8510ddea9a1250a5ac8048865938a475e1
Vani Mittal <vani9045060450@gmail.com>
2023-10-12 18:18:37 +0530

    Inverted pyramid

--- Patterns/Eight.java
+++ Patterns/Eight.java
@@ -14,7 +14,7 @@ public class Eight {
         int n = sc.nextInt();
 
         for (int i = n; i >= 1; i--) {
-            for (int j = 0; j <= n - i - 1; j++) {
+            for (int j = 0; j <= n - i; j++) {
                 System.out.print("  ");
             }
             for (int j = 1; j <= 2 * i - 1; j++) {
