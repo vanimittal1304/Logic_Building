ad1ac34f2e2e7230feaac520e9d5adfd1f9b6b2e
Vani Mittal <vani9045060450@gmail.com>
2023-10-12 10:09:27 +0530

    Square done

---
+++ Patterns/One.java
@@ -0,0 +1,23 @@
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+
+package Patterns;
+
+import java.util.*;
+
+class One {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
